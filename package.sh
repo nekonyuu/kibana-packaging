f688ce24197a215d54bf90104432b39489d7205a
Jonathan Raffre <jonathan.raffre@smile.fr>
2013-12-11 12:23:19 +0100

    adding dashboards to the new path

--- package.sh
+++ package.sh
@@ -12,12 +12,15 @@ cd kibana-*/src/
 
 # Preparing 
 cp -r */ index.html ../../../usr/share/kibana/
+rm -rf ../../../usr/share/kibana/app/dashboards
+cp -r app/dashboards ../../../etc/kibana
 cp -r config.js ../../../etc/kibana
 cd ../../../
 ln -sf /etc/kibana/config.js usr/share/kibana/config.js
+ln -sf /etc/kibana/dashboards usr/share/kibana/app/dashboards
 cp externals/confs/* etc/kibana
 
 cd ..
 
-fpm -n kibana -v ${VERSION} -a all -C kibana-packaging -m "<jonathan.raffre@smile.fr>" --after-install kibana-packaging/kibana.postinstall --description "Kibana 3 - New Generation Log Analyzer" --url 'http://three.kibana.org/' -t deb --config-files etc/kibana/config.js -d httpd -s dir etc usr
+fpm -n kibana -v ${VERSION} -a all -C kibana-packaging -m "<jonathan.raffre@smile.fr>" --after-install kibana-packaging/kibana.postinstall --description "Kibana 3 - New Generation Log Analyzer" --url 'http://three.kibana.org/' -t deb --config-files etc/kibana/config.js -d 'apache2 | nginx' -s dir etc usr
 
